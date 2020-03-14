b28a1aca8ac70e687cba59b7061b336480357116
Jennifer Tian <jennifertian@Jennifers-MacBook-Pro.local>
2020-03-14 13:34:27 -0700

    json

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamVubml0aWFuIiwiYSI6ImNrNTAyaXFvbDBodjMzanFnczg2bzdrem4ifQ.7SBkBkh2630tnMWAR-IxKQ"
---
+++ static/js/logic.js
@@ -0,0 +1,37 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
+
+// We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets
+};
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let incidentData = "Resources/incidents.json";
+
+// Grabbing our GeoJSON data.
+d3.json(incidentData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
